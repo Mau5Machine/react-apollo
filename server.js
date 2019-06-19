31d7f7008e7617789691331022388e15e5862b8a
Christian Martins <christian@labdynamo.com>
2019-06-18 22:42:04 -0400

    made first query and mutation

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: 'variables.env' });
 
-require('dotenv').config({path: 'variables.env'});
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
 
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers')
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('DB Connected'))
-  .catch((err) => console.error(err))
+  .catch((err) => console.error(err));
+
 const app = express();
 
+// Connect schemas with graphql
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }
+));
+app.use('/graphql',
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User
+    }
+  }));
 const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
-
-// mongodb://mau5machine:Fl561stfu9500@mongocloudtest-shard-00-00-izrut.gcp.mongodb.net:27017,mongocloudtest-shard-00-01-izrut.gcp.mongodb.net:27017,mongocloudtest-shard-00-02-izrut.gcp.mongodb.net:27017/test?ssl=true&replicaSet=MongoCloudTest-shard-0&authSource=admin&retryWrites=true&w=majority
